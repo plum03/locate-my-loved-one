9ade6a21071ab358cd32142231b1216ac1ca20b0
Amy Scoville <amy.scoville@gmail.com>
2018-02-16 17:14:25 -0700

    added authform

---
+++ client/src/Components/SignUp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Authform from './AuthForm';
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <Authform />
+            </div>
+        )
+    }
+}
